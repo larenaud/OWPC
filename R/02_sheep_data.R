2e2dd4d4b4f9b5fea02345c791121c41ce62c932
Limoilou-Amélie Renaud <LimoilouARenaud@MacBook-Pro-de-Limoilou-Amelie.local>
2020-02-09 21:58:13 -0500

    adding ghost lambs

--- R/02_sheep_data.R
+++ R/02_sheep_data.R
@@ -1,11 +1,7 @@
-
 getwd()
 load("/Users/LimoilouARenaud/Documents/PhD/Analyses/OWPC/OWPC/cache/surv_data_pop.RData")
 
 head(females)
-
-library(plyr)
-library(dplyr)
 
 females = females[females$code.sr == "1",]
 females$lamb_id = paste(females$ID, females$yr, sep = "-")
