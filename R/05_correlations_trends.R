82145ba3ee6a47eb5970fb57c00007b2b0e28820
yannyritchot <yannyritchot@gmail.com>
2020-02-10 15:46:34 -0500

    TEST

--- R/05_correlations_trends.R
+++ R/05_correlations_trends.R
@@ -1,1 +1,12 @@
-blablbla 
+library(googledrive)
+setwd("~/uSherbrooke/Hiver 2020/NDVI/OWPC")
+drive_download("OWPC/Analyses/data/pheno_ram2.csv", overwrite=T)
+
+
+pheno<-read.csv("pheno_ram2.csv", header=T, sep=",")
+mass<-mass[,c("yr","ID", "JJ", "age", "adjwt", "sex")]
+
+write.csv(mass, "Ram_mass.csv", row.names = FALSE)
+drive_upload("Ram_mass.csv", path = "OWPC/Analyses/data/Ram_mass.csv", overwrite = T)
+
+
