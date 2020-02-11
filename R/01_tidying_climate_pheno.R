17644191871fdbb38e911501b4b2cb07ce0bd98c
Pier-Olivier Cusson <po.cusson94@gmail.com>
2020-02-11 10:40:46 -0500

    # Ajout du code pour calculer les valeurs saisonieres de PDO et SOI

--- R/01_tidying_climate_pheno.R
+++ R/01_tidying_climate_pheno.R
@@ -125,3 +125,52 @@ legend("topleft", legend = c("NDVI","EVI","LAI","GPP","Snow","PSNNET","FPAR"),
 
 
 >>>>>>> 5855572c1b719f96e9bf0a7eac0ba8a4bb789b43
+
+##### Calculating seasonal PDO and SOI values from monthly data ##########################
+
+# Cleaning R
+#rm(list=ls(all=TRUE))
+
+# Set working directory
+#setwd("C:/Users/Proprietaire/Documents/uni poc/Phd/OWPC/Analyses")
+
+library(googledrive)
+
+# Download database from google drive and save it in previously set working directory
+drive_download("OWPC/Analyses/data/Climat/monthly_climate_ram",type = "csv",overwrite = T)
+# Load database in R environment
+data<-read.csv("monthly_climate_ram.csv")
+
+# See values for December PDOs
+data$PDO.DEC
+# Create column with PDO values in december of the previous year
+data$PDO.DEC.PREV.YR<-c(-1.63, 0.52, -0.93, 2.10, 0.33, -0.17, 0.20, 0.14, -0.58, -0.87, 0.08, -1.21, -1.79,-0.48, -0.41, 2.51, 1.01)
+# Calculate mean PDO in winter
+data$PDO.winter<-(data$PDO.DEC.PREV.YR+data$PDO.JAN+data$PDO.FEB+data$PDO.MAR)/4
+# Calculate mean PDO in spring
+data$PDO.spring<-(data$PDO.APR+data$PDO.MAY)/2
+# Calculate mean PDO in summer
+data$PDO.summer<-(data$PDO.JUN+data$PDO.JUL+data$PDO.AUG+data$PDO.SEP)/4
+# Calculate mean PDO in fall
+data$PDO.fall<-(data$PDO.OCT+data$PDO.NOV)/2
+
+# See values for December SOIs
+data$SOI.DEC
+# Create column with SOI values in december of the previous year
+data$SOI.DEC.PREV.YR<-c(0.79,0.77,-1.06,-1.30,0.92,-0.94,0.01,-0.39,1.49,1.43,-0.95,2.90,2.45,-0.77,-0.05,-0.66,-1.00)
+# Calculate mean SOI in winter
+data$SOI.winter<-(data$SOI.DEC.PREV.YR+data$SOI.JAN+data$SOI.FEB+data$SOI.MAR)/4
+# Calculate mean SOI in spring
+data$SOI.spring<-(data$SOI.APR+data$SOI.MAY)/2
+# Calculate mean SOI in summer
+data$SOI.summer<-(data$SOI.JUN+data$SOI.JUL+data$SOI.AUG+data$SOI.SEP)/4
+# Calculate mean SOI in fall
+data$SOI.fall<-(data$SOI.OCT+data$SOI.NOV)/2
+
+# Create new dataframe with seasonal values of PDO and SOI
+season_climate_ram<-data.frame(data$yr,data$PDO.winter,data$PDO.spring,data$PDO.summer,data$PDO.fall,data$SOI.winter,data$SOI.spring,data$SOI.summer,data$SOI.fall)
+colnames(season_climate_ram)<-c("yr","PDO.winter","PDO.spring","PDO.summer","PDO.fall","SOI.winter","SOI.spring","SOI.summer","SOI.fall")
+
+# Save new dataframe in a csv file and upload it to google drive
+#write.csv(season_climat_ram,"season_climate_ram.csv",row.names=FALSE)
+#drive_upload("season_climate_ram.csv",path = "OWPC/Analyses/data/Climat",name = "season_climate_ram")
