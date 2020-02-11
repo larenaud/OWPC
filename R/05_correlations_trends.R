4562f0fa43926f8883d2253fd986875ff9c5963e
Pier-Olivier Cusson <po.cusson94@gmail.com>
2020-02-11 15:38:34 -0500

    # Ajout d'un graphique des variations temporelles du PDO et du SOI

--- R/05_correlations_trends.R
+++ R/05_correlations_trends.R
@@ -62,4 +62,53 @@ dat<-df
   # Precision on correlation values
   symnum(C)
   
+##### Seasonal climate trends ####################################################
+library(googledrive)
+
+#drive_download("OWPC/Analyses/data/Climat/season_climate_ram",type="csv",overwrite = T)
+data<-read.csv("season_climate_ram.csv")
   
+library(ggplot2)
+library(cowplot)
+
+# Theme object for ggplot graphs
+fig <- theme_bw() + theme(panel.grid.minor=element_blank(), panel.grid.major=element_blank(), panel.background=element_blank()) + 
+    theme(strip.background=element_blank(), strip.text.y = element_text()) + theme(legend.background=element_blank()) + 
+    theme(legend.key=element_blank()) + theme(panel.border = element_blank())+ theme(legend.position = "none")+
+    theme(axis.line = element_line(colour="black"))+theme(axis.text = element_text(size=12))+
+    theme(axis.title = element_text(size=12))+theme(title = element_text(size=12))
+  
+# PDO trends
+WinterPDO<-ggplot(data,aes(yr,PDO.winter)) + xlab("Year") + ylab("") + ggtitle("Winter PDO") + 
+  scale_y_continuous(breaks=seq(-2,2,1),limits=c(-2,2.5)) + geom_line() + geom_point() + fig
+SpringPDO<-ggplot(data,aes(yr,PDO.spring)) + xlab("Year") + ylab("") + ggtitle("Spring PDO") + 
+  scale_y_continuous(breaks=seq(-2,2,1),limits=c(-2,2.5)) + geom_line() + geom_point() + fig
+SummerPDO<-ggplot(data,aes(yr,PDO.summer)) + xlab("Year") + ylab("") + ggtitle("Summer PDO") + 
+  scale_y_continuous(breaks=seq(-2,2,1),limits=c(-2,2.5)) + geom_line() + geom_point() + fig
+FallPDO<-ggplot(data,aes(yr,PDO.fall)) + xlab("Year") + ylab("") + ggtitle("Fall PDO") + 
+  scale_y_continuous(breaks=seq(-2,2,1),limits=c(-2,2.5)) + geom_line() + geom_point() + fig
+PDO<-plot_grid(WinterPDO,SpringPDO,SummerPDO,FallPDO,ncol=1)
+PDO
+  
+#SOI trends
+WinterSOI<-ggplot(data,aes(yr,SOI.winter)) + xlab("Year") + ylab("") + ggtitle("Winter SOI") + 
+  scale_y_continuous(breaks=seq(-2,2,1),limits=c(-2,2.5)) + geom_line() + geom_point() + fig
+SpringSOI<-ggplot(data,aes(yr,SOI.spring)) + xlab("Year") + ylab("") + ggtitle("Spring SOI") + 
+  scale_y_continuous(breaks=seq(-2,2,1),limits=c(-2,2.5)) + geom_line() + geom_point() + fig
+SummerSOI<-ggplot(data,aes(yr,SOI.summer)) + xlab("Year") + ylab("") + ggtitle("Summer SOI") + 
+  scale_y_continuous(breaks=seq(-2,2,1),limits=c(-2,2.5)) + geom_line() + geom_point() + fig
+FallSOI<-ggplot(data,aes(yr,SOI.fall)) + xlab("Year") + ylab("") + ggtitle("Fall SOI") + 
+  scale_y_continuous(breaks=seq(-2,2,1),limits=c(-2,2.5)) + geom_line() + geom_point() + fig
+SOI<-plot_grid(WinterSOI,SpringSOI,SummerSOI,FallSOI,ncol=1)
+SOI
+
+# Combine PDO and SOI 
+Clim<-plot_grid(PDO,SOI,ncol=2)
+Clim
+
+# Save ggplot to pdf
+#ggsave2("C:/Users/Proprietaire/Documents/uni poc/Phd/OWPC/Analyses/Temp/Seasonal_climate_trends.pdf",
+#        plot= Clim, device= cairo_pdf, scale = 0.8, width = 20, height = 30, units = "cm", dpi = 600)
+
+
+##### 
