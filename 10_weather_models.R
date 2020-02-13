244b0c4ead2f1a1f60b6731307102653883cd1f3
yannyritchot <yannyritchot@gmail.com>
2020-02-13 16:05:05 -0500

    Adding repro

--- 10_weather_models.R
+++ 10_weather_models.R
@@ -98,114 +98,84 @@ df_surv[c("MassSpring","MassAutumn","T.Win","P.Win","T.SPRING" ,"P.SPRING", "T.S
 
 colnames(df_surv)
 mod.l <- list()
-mod.l$base <- glmer(alive_t1 ~ -1 +  pred+ (1|ID) + (1|yr), 
-                    data=df_surv, 
-                    family="binomial",
-                    control = glmerControl(optimizer="bobyqa", 
-                                           optCtrl = list(maxfun = 1000000))) 
+mod.l$ageclass <- glm(alive_t1 ~ -1 + ageClass +  pred, 
+                      data=df_surv, 
+                      family="binomial") 
+
+
+mod.l$base <- glm(alive_t1 ~ -1 +  pred, 
+                  data=df_surv, 
+                  family="binomial") 
 
 # Winter
-mod.l$P.T.Win <- glmer(alive_t1 ~ -1 + ageClass/P.Win+ageClass/T.Win +  pred+ (1|ID) + (1|yr), 
-                            data=df_surv, 
-                            family="binomial",
-                            control = glmerControl(optimizer="bobyqa", 
-                                                   optCtrl = list(maxfun = 2000000)))
-
-
-mod.l$PxT.Win <- glmer(alive_t1 ~ -1 + ageClass/(P.Win*T.Win) +  pred+ (1|ID) + (1|yr), 
-                       data=df_surv, 
-                       family="binomial",
-                       control = glmerControl(optimizer="bobyqa", 
-                                              optCtrl = list(maxfun = 2000000)))
-
-mod.l$T.Win <- glmer(alive_t1 ~ -1 + ageClass/T.Win +  pred+ (1|ID) + (1|yr), 
-                       data=df_surv, 
-                       family="binomial",
-                       control = glmerControl(optimizer="bobyqa", 
-                                              optCtrl = list(maxfun = 2000000)))
-
-mod.l$P.Win <- glmer(alive_t1 ~ -1 + ageClass/P.Win +  pred+ (1|ID) + (1|yr), 
-                       data=df_surv, 
-                       family="binomial",
-                       control = glmerControl(optimizer="bobyqa", 
-                                              optCtrl = list(maxfun = 2000000)))
-
-  # Spring
-
-mod.l$P.T.Spring <- glmer(alive_t1 ~ -1 + ageClass/P.SPRING+ageClass/T.SPRING +  pred+ (1|ID) + (1|yr), 
-                       data=df_surv, 
-                       family="binomial",
-                       control = glmerControl(optimizer="bobyqa", 
-                                              optCtrl = list(maxfun = 2000000)))
-
-mod.l$PxT.Spring <- glmer(alive_t1 ~ -1 + ageClass/(P.SPRING*T.SPRING) +  pred+ (1|ID) + (1|yr), 
-                       data=df_surv, 
-                       family="binomial",
-                       control = glmerControl(optimizer="bobyqa", 
-                                              optCtrl = list(maxfun = 2000000)))
-
-mod.l$T.Spring <- glmer(alive_t1 ~ -1 + ageClass/T.SPRING +  pred+ (1|ID) + (1|yr), 
+mod.l$P.T.Win <- glm(alive_t1 ~ -1 + ageClass/P.Win+ageClass/T.Win +  pred, 
                      data=df_surv, 
-                     family="binomial",
-                     control = glmerControl(optimizer="bobyqa", 
-                                            optCtrl = list(maxfun = 2000000)))
+                     family="binomial")
 
-mod.l$P.Spring <- glmer(alive_t1 ~ -1 + ageClass/P.SPRING +  pred+ (1|ID) + (1|yr), 
-                    data=df_surv, 
-                    family="binomial",
-                    control = glmerControl(optimizer="bobyqa", 
-                                           optCtrl = list(maxfun = 2000000)))
 
-  # Summer
-mod.l$P.T.Summer <- glmer(alive_t1 ~ -1 + ageClass/P.SUMMER+ageClass/T.SUMMER +  pred+ (1|ID) + (1|yr), 
-                       data=df_surv, 
-                       family="binomial",
-                       control = glmerControl(optimizer="bobyqa", 
-                                              optCtrl = list(maxfun = 2000000)))
-
-mod.l$PxT.Summer <- glmer(alive_t1 ~ -1 + ageClass/(P.SUMMER*T.SUMMER) +  pred+ (1|ID) + (1|yr), 
-                       data=df_surv, 
-                       family="binomial",
-                       control = glmerControl(optimizer="bobyqa", 
-                                              optCtrl = list(maxfun = 2000000)))
-
-mod.l$T.Summer <- glmer(alive_t1 ~ -1 + ageClass/T.SUMMER +  pred+ (1|ID) + (1|yr), 
+mod.l$PxT.Win <- glm(alive_t1 ~ -1 + ageClass/(P.Win*T.Win) +  pred, 
                      data=df_surv, 
-                     family="binomial",
-                     control = glmerControl(optimizer="bobyqa", 
-                                            optCtrl = list(maxfun = 2000000)))
-  # Fall
+                     family="binomial")
 
-mod.l$P.Fall <- glmer(alive_t1 ~ -1 + ageClass/P.AUT +  pred+ (1|ID) + (1|yr), 
+mod.l$T.Win <- glm(alive_t1 ~ -1 + ageClass/T.Win +  pred, 
+                   data=df_surv, 
+                   family="binomial")
+
+mod.l$P.Win <- glm(alive_t1 ~ -1 + ageClass/P.Win +  pred, 
+                   data=df_surv, 
+                   family="binomial")
+
+# Spring
+
+mod.l$P.T.Spring <- glm(alive_t1 ~ -1 + ageClass/P.SPRING+ageClass/T.SPRING +  pred, 
+                        data=df_surv, 
+                        family="binomial")
+
+mod.l$PxT.Spring <- glm(alive_t1 ~ -1 + ageClass/(P.SPRING*T.SPRING) +  pred, 
+                        data=df_surv, 
+                        family="binomial")
+
+mod.l$T.Spring <- glm(alive_t1 ~ -1 + ageClass/T.SPRING +  pred, 
+                      data=df_surv, 
+                      family="binomial")
+
+mod.l$P.Spring <- glm(alive_t1 ~ -1 + ageClass/P.SPRING +  pred, 
+                      data=df_surv, 
+                      family="binomial")
+
+# Summer
+mod.l$P.T.Summer <- glm(alive_t1 ~ -1 + ageClass/P.SUMMER+ageClass/T.SUMMER +  pred, 
+                        data=df_surv, 
+                        family="binomial")
+
+mod.l$PxT.Summer <- glm(alive_t1 ~ -1 + ageClass/(P.SUMMER*T.SUMMER) +  pred, 
+                        data=df_surv, 
+                        family="binomial")
+
+mod.l$T.Summer <- glm(alive_t1 ~ -1 + ageClass/T.SUMMER +  pred, 
+                      data=df_surv, 
+                      family="binomial")
+# Fall
+
+mod.l$P.Fall <- glm(alive_t1 ~ -1 + ageClass/P.AUT +  pred, 
                     data=df_surv, 
-                    family="binomial",
-                    control = glmerControl(optimizer="bobyqa", 
-                                           optCtrl = list(maxfun = 2000000)))
+                    family="binomial")
 
-mod.l$P.T.Fall <- glmer(alive_t1 ~ -1 + ageClass/P.AUT+ageClass/T.AUT +  pred+ (1|ID) + (1|yr), 
-                       data=df_surv, 
-                       family="binomial",
-                       control = glmerControl(optimizer="bobyqa", 
-                                              optCtrl = list(maxfun = 2000000)))
+mod.l$P.T.Fall <- glm(alive_t1 ~ -1 + ageClass/P.AUT+ageClass/T.AUT +  pred, 
+                      data=df_surv, 
+                      family="binomial")
 
-mod.l$PxT.Fall <- glmer(alive_t1 ~ -1 + ageClass/(P.AUT*T.AUT) +  pred+ (1|ID) + (1|yr), 
-                       data=df_surv, 
-                       family="binomial",
-                       control = glmerControl(optimizer="bobyqa", 
-                                              optCtrl = list(maxfun = 2000000)))
+mod.l$PxT.Fall <- glm(alive_t1 ~ -1 + ageClass/(P.AUT*T.AUT) +  pred, 
+                      data=df_surv, 
+                      family="binomial")
 
-mod.l$T.Fall <- glmer(alive_t1 ~ -1 + ageClass/T.AUT +  pred+ (1|ID) + (1|yr), 
-                     data=df_surv, 
-                     family="binomial",
-                     control = glmerControl(optimizer="bobyqa", 
-                                            optCtrl = list(maxfun = 2000000)))
-
-mod.l$P.Fall <- glmer(alive_t1 ~ -1 + ageClass/P.AUT +  pred+ (1|ID) + (1|yr), 
+mod.l$T.Fall <- glm(alive_t1 ~ -1 + ageClass/T.AUT +  pred, 
                     data=df_surv, 
-                    family="binomial",
-                    control = glmerControl(optimizer="bobyqa", 
-                                           optCtrl = list(maxfun = 2000000)))
+                    family="binomial")
 
+mod.l$P.Fall <- glm(alive_t1 ~ -1 + ageClass/P.AUT +  pred, 
+                    data=df_surv, 
+                    family="binomial")
 
 
 ## exporting AIC table
@@ -229,3 +199,101 @@ getwd()
 
 results_weather_surv<- write.csv(results.T.Win, file = "C:/Users/Yanny/Documents/uSherbrooke/Hiver 2020/NDVI/OWPC/OWPC/results_weather_surv.xls", row.names = TRUE)
 
+
+# Repro
+names(weather_fec)
+names(sheep)
+colnames(df_fec)
+tmp <- sheep[, c("yr", "ID", "MassAutumn", "pred", "true_repro")]
+tmp$yr <-as.numeric(as.character(tmp$yr))
+
+tmp$yr <- tmp$yr + 1
+
+tmp <- tmp %>% 
+  rename(MassAutumn_tm1= MassAutumn, 
+         pred_tm1= pred, 
+         true_repro_tm1 = true_repro)
+
+df_fec<- merge(tmp, 
+               weather_fec, 
+               by.x= c("yr"), 
+               by.y=c("yr"), 
+               all.y = T)
+##--- NE FONCTIONNE PAS -----
+
+str(df_fec)
+df_fec$yr <- as.factor(df_fec$yr)
+
+
+# remove age classes 0-1-2 
+df_fec <- subset(df_fec, ageClass %in% c(37,8))
+df_fec$ageClass <- droplevels(df_fec$ageClass)
+df_fec$pred_tm1 <- as.factor(df_fec$pred_tm1)
+
+mod.l <- list()
+mod.l$base <- glmer(raw_repro ~ -1 + MassAutumn_tm1 + (1|ID) + (1|yr), # here write the model
+                    data=df_fec, 
+                    family="binomial",
+                    control = glmerControl(optimizer="bobyqa", 
+                                           optCtrl = list(maxfun = 2000000))) 
+mod.l$summerPDOSOI <- glmer(raw_repro ~ -1 + ageClass/PDO.summer_fec+ageClass/(PDOSOI_summer) +  MassAutumn_tm1 + (1|ID) + (1|yr), # here write the model
+                            data=df_fec, 
+                            family="binomial",
+                            control = glmerControl(optimizer="bobyqa", 
+                                                   optCtrl = list(maxfun = 2000000))) 
+mod.l$summerPDO <- glmer(raw_repro ~ -1 + ageClass/PDO.summer_fec +  MassAutumn_tm1 + (1|ID) + (1|yr), # here write the model
+                         data=df_fec, 
+                         family="binomial",
+                         control = glmerControl(optimizer="bobyqa", 
+                                                optCtrl = list(maxfun = 2000000))) 
+mod.l$summer_int <- glmer(raw_repro ~ -1 + ageClass/PDOSOI_summer +  MassAutumn_tm1 + (1|ID) + (1|yr), # here write the model
+                          data=df_fec, 
+                          family="binomial",
+                          control = glmerControl(optimizer="bobyqa", 
+                                                 optCtrl = list(maxfun = 1000000))) 
+mod.l$fallPDOSOI <- glmer(raw_repro ~ -1 + ageClass/PDO.fall_fec+ageClass/(PDOSOI_fall) +  MassAutumn_tm1 + (1|ID) + (1|yr), # here write the model
+                          data=df_fec, 
+                          family="binomial",
+                          control = glmerControl(optimizer="bobyqa", 
+                                                 optCtrl = list(maxfun = 1000000))) 
+mod.l$fallPDO <- glmer(raw_repro ~ -1 + ageClass/PDO.fall_fec +  MassAutumn_tm1 +  (1|ID) + (1|yr), # here write the model
+                       data=df_fec, 
+                       family="binomial",
+                       control = glmerControl(optimizer="bobyqa", 
+                                              optCtrl = list(maxfun = 1000000))) 
+mod.l$fall_int <- glmer(raw_repro ~ -1 + ageClass/PDOSOI_fall +  MassAutumn_tm1 +  (1|ID) + (1|yr), # here write the model
+                        data=df_fec, 
+                        family="binomial",
+                        control = glmerControl(optimizer="bobyqa", 
+                                               optCtrl = list(maxfun = 1000000))) 
+
+mod.l$winterPDOSOI <- glmer(raw_repro ~ -1 + ageClass/PDO.winter_fec+ageClass/(PDOSOI_winter) +  MassAutumn_tm1 +  (1|ID) + (1|yr), # here write the model
+                            data=df_fec, 
+                            family="binomial",
+                            control = glmerControl(optimizer="bobyqa", 
+                                                   optCtrl = list(maxfun = 1000000))) 
+mod.l$winterPDO<- glmer(raw_repro ~ -1 + ageClass/PDO.winter_fec +  MassAutumn_tm1 +  (1|ID) + (1|yr), # here write the model
+                        data=df_fec, 
+                        family="binomial",
+                        control = glmerControl(optimizer="bobyqa", 
+                                               optCtrl = list(maxfun = 1000000))) 
+mod.l$winter_int <- glmer(raw_repro ~ -1 + ageClass/PDOSOI_winter +  MassAutumn_tm1 +  (1|ID) + (1|yr), # here write the model
+                          data=df_fec, 
+                          family="binomial",
+                          control = glmerControl(optimizer="bobyqa", 
+                                                 optCtrl = list(maxfun = 2000000))) 
+mod.l$springPDOSOI <- glmer(raw_repro ~ -1 + ageClass/PDO.spring_fec+ageClass/(PDOSOI_spring) +  MassAutumn_tm1 + (1|ID) + (1|yr), # here write the model
+                            data=df_fec, 
+                            family="binomial",
+                            control = glmerControl(optimizer="bobyqa", 
+                                                   optCtrl = list(maxfun = 1000000))) 
+mod.l$springPDO<- glmer(raw_repro ~ -1 + ageClass/PDO.spring_fec +  MassAutumn_tm1 +  (1|ID) + (1|yr), # here write the model
+                        data=df_fec, 
+                        family="binomial",
+                        control = glmerControl(optimizer="bobyqa",
+                                               optCtrl = list(maxfun = 2000000))) 
+mod.l$spring_int <- glmer(raw_repro ~ -1 +  ageClass/PDOSOI_spring +  MassAutumn_tm1 +  (1|ID) + (1|yr), # here write the model
+                          data=df_fec, 
+                          family="binomial",
+                          control = glmerControl(optimizer=list("bobyqa"),
+                                                 optCtrl = list(maxfun = 1000000))) 
