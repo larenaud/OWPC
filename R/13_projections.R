631d59e8c97e8406c645d0e0393e9e66ab77304d
Limoilou Renaud <60702430+larenaud@users.noreply.github.com>
2020-05-14 17:28:35 -0400

    # our first projection

--- R/13_projections.R
+++ R/13_projections.R
@@ -23,7 +23,6 @@ matrices <- load("data/Matrice_All_Years.RData")
 
 
 
-
 # get final models to extract beta coefficients 
 # survival 
 load("graph/final_surv.Rdata")
@@ -33,7 +32,29 @@ surv <- glm(alive_t1 ~ -1 + ageClass/PC2Tim + pred,
                  family = "binomial",
                  df_surv) 
 
-load("graph/final_raw.Rdata")
+summary(surv)
+
+
+
+
+# here replace by variable with the greated loading (score) on PC2 for timing (green-up date) # ex. GPPsurvT or PSNNETsurvT
+
+# data with all necessary SD for projections 
+load("cache/dataProjectionsSURV.Rdata")
+# Rename scaled data frame and remove years with missing data for climate
+df_surv<-droplevels(subset(fullSurvDataScled,!(yr %in% c("1999","2000","2016"))))
+# Remove unnecessary objects for the environment
+rm(clim_surv,pheno_surv,weather_surv,sheep_data,dataSurvUnscld,dataSurvScld)
+
+surv <- glm(alive_t1 ~ -1 + ageClass/GPPsurvT + pred, 
+            family = "binomial",
+            df_surv) 
+
+summary(surv) # results are similar than with PC
+
+
+
+load("graph/final_raw.Rdata") # keep this model as it is 
 
 raw <- glmer(raw_repro ~ -1 + ageClass/TWin + MassAutumn_tm1 + (1 | ID), 
                   family = "binomial", 
@@ -43,7 +64,7 @@ raw <- glmer(raw_repro ~ -1 + ageClass/TWin + MassAutumn_tm1 + (1 | ID),
 
 
 
-load("graph/final_true_models.Rdata")
+load("graph/final_true_models.Rdata") # keep this model as it is (if still final model)
 true <- glmer(true_repro ~ -1 + ageClass/(TWin * PWin) + MassAutumn_tm1 + (1 |ID), 
                    df_fec, 
                    family = "binomial",
@@ -57,7 +78,11 @@ plot(df_fec$TWin ~ df_fec$yr)
 
 # extract model coefficients with uncertainty and generate NEW predicted leslie matrix 
 
-newLeslie <- function(survm=surv, fec = raw, env = newdata[1,]){
+# do matrix with true reproduction only ! 
+
+
+
+newLeslie <- function(survm=surv, fec = true, env = newdata[1,]){ # here changed for fec = true
  # ceci sort les bêta des modèles 
   S = predict(survm, newdata = data.frame(ageClass = c("0","1","2","37","8"), env), type = "link", se.fit = T)
   myfun <- function(x) predict(x,newdata=data.frame(ageClass = c("3","48","9"), env),type="link",re.form=NA)
@@ -94,70 +119,87 @@ return(L)
 
 
 # projections per se 
-# this is the actual data # build fictive dataframes to predict on 
 
-# use unscaled variables LATER
-
-load("cache/dataSurvivalModels.Rdata")
-load("cache/dataFecundityModels.RData")
+# let's way we want to simulate what happens to lambda under different scenarios of temperature and green-up date. 
+# extract these variables as they were in the orignial data 
 
 
+load("cache/dataProjectionsSURV.RData")
 
-q.SD<-sd(dt3$snow_log_up_jul)
-q.MEAN<-mean(dt3$snow_log_up_jul)
-newd$snow<-(newd[,"snow_z"]*q.SD) + q.MEAN
+df_surv<-droplevels(subset(fullSurvDataScled,!(yr %in% c("1999","2000","2016"))))# n= 448
+SD.surv <- data.SD
+
+load("cache/dataProjectionsFEC.RData")
+df_fec<-droplevels(subset(fullFecDataScld,!(yr %in% c("1999","2000","2001")))) # n= 274
+SD.fec <- data.SD
 
 
+# this is the actual data 
+# build fictive dataframes to predict lambda on 
+# tricky : Temperature variable in df_fec isn't the same (time lag) than in df_surv. BE CAREFUL
 
-# no good
-
-env.now <- data.frame(TWin = mean(df_fec$TWin, na.rm = T),
-                      T.WIN = mean(df_surv$T.WIN , na.rm = T),
-                      PC2Tim = mean(df_surv$PC2Tim, na.rm = T),
+env.now <- data.frame(TWin = mean(df_fec$TWin, na.rm = T), 
                       PWin = mean(df_fec$PWin, na.rm = T),
-                      # dont if put PWin?? 
-                      MassAutumn_tm1 = mean(df_fec$MassAutumn_tm1, na.rm = T),
+                      MassAutumn_tm1 = mean(df_fec$MassAutumn_tm1, na.rm = T), # variables for reproduction model 
+                      T.WIN = mean(df_surv$T.WIN , na.rm = T),
+                      GPPsurvT = mean(df_surv$GPPsurvT, na.rm = T), # variables for survival model # here use original variable from PC loadings (ESM, first tables)
                       pred = "0")
 
-# this is the data under climate change HOWEVER IT IS NOW SCALED
-env.future <- data.frame(TWin = mean(df_fec$TWin, na.rm = T)+1.5,
-                         T.WIN = mean(df_surv$T.WIN , na.rm = T)+1.5,
-                         PC2Tim = mean(df_surv$PC2Tim, na.rm = T),
-                         PWin = mean(df_fec$PWin, na.rm = T),
-                         # dont if put PWin?? 
-                         MassAutumn_tm1 = mean(df_fec$MassAutumn_tm1, na.rm = T),
-                         pred = "0")
 
 
-# raw fec # need 10 000
-lambda.raw.now = 
-sapply(1:10, function(it){
-  bob = eigen.analysis(newLeslie(fec = raw,env = env.now))
-  return(bob$lambda1)
-})
-hist(lambda.raw.now)
+# this is the data under climate change 
+
+# ATTENTION use EACH VARIABLE'S SD nd add this SD to 1.5 so that everything is on the same SCALE
 
 
+# Scaling is done like this :
+# newd$snow_z<-(data.MEAN["SummerEVI"]- data.MEAN)/data.SD
 
-lambda.raw.future = 
-  sapply(1:10, function(it){ # sapply fait des itérations 
-    bob = eigen.analysis(newLeslie(fec = raw,env = env.future))
-    return(bob$lambda1)
-  })
-hist(lambda.raw.future)
+
+# ex when you "unscale" a scaled variable you do this : 
+# newd$snow<-(data.MEAN["SummerEVI"]*data.SD) + data.MEAN
+
+
+# now we want to add 1.5 to TEMPERATURE ONLY on the scale of original variable, by dividing 1.5 by its SD (mean = 0)
+env.future <- data.frame(TWin = mean(df_fec$TWin, na.rm = T) + 1.5/SD.fec["TWin"], 
+                      PWin = mean(df_fec$PWin, na.rm = T),
+                      MassAutumn_tm1 = mean(df_fec$MassAutumn_tm1, na.rm = T), 
+                      T.WIN = mean(df_surv$T.WIN , na.rm = T)+ 1.5/SD.surv["T.WIN"],
+                      GPPsurvT = mean(df_surv$GPPsurvT, na.rm = T),
+                      pred = "0")
+
+
+# MIGHT WANT TO REMOVE THE "RAW" PART ?? 
+
+
+# # raw fec in actual environment  
+# lambda.raw.now = 
+# sapply(1:1000, function(it){
+#   bob = eigen.analysis(newLeslie(fec = raw,env = env.now))# need minimum 1000 iterations. This is JUST A TEST  
+#   return(bob$lambda1)
+# })
+# hist(lambda.raw.now)
+# 
+# 
+# lambda.raw.future = 
+#   sapply(1:1000, function(it){
+#     bob = eigen.analysis(newLeslie(fec = raw,env = env.future))
+#     return(bob$lambda1)
+#   })
+# hist(lambda.raw.future)
 
 
 # make TRUE fecondity 
 
 lambda.true.now = 
-  sapply(1:10, function(it){
+  sapply(1:1000, function(it){
     bob = eigen.analysis(newLeslie(fec = true,env = env.now))
     return(bob$lambda1)
   })
 hist(lambda.true.now)
 
 lambda.true.future = 
-  sapply(1:10, function(it){ 
+  sapply(1:1000, function(it){ 
     bob = eigen.analysis(newLeslie(fec = true,env = env.future))
     return(bob$lambda1)
   })
@@ -171,7 +213,7 @@ lambda <- data.frame(lambda.true.now,
 
 
 
-# figure temp projection  -----------------------------------------------------------------
+# projection figure FOR TEMPERATURE  -----------------------------------------------------------------
 
 projectionTemp <- ggplot(lambda, aes(lambda.true.now, fill = "true.now")) + 
   geom_density(alpha = 0.5) + 
@@ -186,102 +228,30 @@ projectionTemp
 
 
 
-# green-up projections ----------------------------------------------------
-
-# 19 day advance in 60 yrs. 
+# projection figure FOR GREENUP ----------------------------------------------------
 
 
-
-# get final models to extract beta coefficients # here try only 1 var of pc  
-# survival 
-load("graph/final_surv.Rdata")
+# final models should be the same than previous projection (but updated)
 
 
-surv <- glm(alive_t1 ~ -1 + ageClass/NDVIsurvT + pred, # will have to change pc2tim to other variable thus dataframe... 
-            family = "binomial",
-            df_surv) 
-
-load("graph/final_raw.Rdata")
-
-raw <- glmer(raw_repro ~ -1 + ageClass/TWin + MassAutumn_tm1 + (1 | ID), 
-             family = "binomial", 
-             df_fec, 
-             control = glmerControl(optimizer="bobyqa", 
-                                    optCtrl = list(maxfun = 2000000)))
+# leslie function is the same 
 
 
-
-load("graph/final_true_models.Rdata")
-true <- glmer(true_repro ~ -1 + ageClass/(TWin * PWin) + MassAutumn_tm1 + (1 |ID), 
-              df_fec, 
-              family = "binomial",
-              control = glmerControl(optimizer="bobyqa", 
-                                     optCtrl = list(maxfun = 2000000)))
-
-
-plot(df_fec$TWin ~ df_fec$yr)
-
-
-
-# extract model coefficients with uncertainty and generate NEW predicted leslie matrix 
-
-newLeslie <- function(survm=surv, fec = raw, env = newdata[1,]){
-  # ceci sort les bêta des modèles 
-  S = predict(survm, newdata = data.frame(ageClass = c("0","1","2","37","8"), env), type = "link", se.fit = T)
-  myfun <- function(x) predict(x,newdata=data.frame(ageClass = c("3","48","9"), env),type="link",re.form=NA)
-  F1 = as.numeric(bootMer(fec,myfun, nsim = 1)$t) # rajoute l'incetitude ; va juste chercher le chiffre qui nous intéresse dans t 
-  S = rnorm(5, S$fit, S$se.fit)# rajoute de l'incertitude
-  # ceci crée une matrice vide 
-  L <- matrix(0, nrow=9, ncol=9)
-  
-  # survival is lamb overwinter survival * reproduction de la femelle /2 pour compter juste des bébés femelles 
-  L[1,3] <- inv.logit(S[1]) * inv.logit(F1[1])*0.5 # doit fitter avec l'ordre des classes d'age ci-haut # la moitié des agneaux sont femelles 
-  L[1,4] <- inv.logit(S[1]) * inv.logit(F1[2])*0.5 # same
-  L[1,5] <- inv.logit(S[1]) * inv.logit(F1[2])*0.5
-  L[1,6] <- inv.logit(S[1]) * inv.logit(F1[2])*0.5
-  L[1,7] <- inv.logit(S[1]) * inv.logit(F1[2])*0.5
-  L[1,8] <- inv.logit(S[1]) * inv.logit(F1[2])*0.5
-  L[1,9] <- inv.logit(S[1]) * inv.logit(F1[3])*0.5
-  
-  
-  L[2,1] <- inv.logit(S[2])
-  L[3,2] <- inv.logit(S[3])    
-  L[4,3] <- inv.logit(S[4]) 
-  L[5,4] <- inv.logit(S[4])   
-  L[6,5] <- inv.logit(S[4])    
-  L[7,6] <- inv.logit(S[4])  
-  L[8,7] <- inv.logit(S[4])   
-  L[9,8] <- inv.logit(S[5])   
-  L[9,9] <- inv.logit(S[5])  
-  
-  
-  return(L)
-} 
-
-
-#unscale
-# q.SD<-sd(dt3$snow_log_up_jul)
-# q.MEAN<-mean(dt3$snow_log_up_jul)
-# newd$snow<-(newd[,"snow_z"]*q.SD) + q.MEAN
-# 
-
-
-env.now <- data.frame(TWin = mean(dataFecUnscld$TWin, na.rm = T),
-                      T.WIN = mean(dataSurvUnscld$T.WIN , na.rm = T),
-                      NDVIsurvT = mean(dataSurvUnscld$NDVIsurvT, na.rm = T),
-                      PWin = mean(dataFecUnscld$PWin, na.rm = T),
-                      # dont if put PWin?? 
-                      MassAutumn_tm1 = mean(dataFecUnscld$MassAutumn_tm1, na.rm = T),
+env.now <- data.frame(TWin = mean(df_fec$TWin, na.rm = T), 
+                      PWin = mean(df_fec$PWin, na.rm = T),
+                      MassAutumn_tm1 = mean(df_fec$MassAutumn_tm1, na.rm = T), # variables for reproduction model 
+                      T.WIN = mean(df_surv$T.WIN , na.rm = T),
+                      GPPsurvT = mean(df_surv$GPPsurvT, na.rm = T), # variables for survival model # here use original variable from PC loadings (ESM, first tables)
                       pred = "0")
 
-# this is the data under climate change HOWEVER IT IS NOW SCALED
-env.future <- data.frame(TWin = mean(dataFecUnscld$TWin, na.rm = T),
-                         T.WIN = mean(dataSurvUnscld$T.WIN , na.rm = T),
-                         NDVIsurvT = mean(dataSurvUnscld$NDVIsurvT, na.rm = T) - 19, # to change 
-                         PWin = mean(dataFecUnscld$PWin, na.rm = T),
-                         # dont if put PWin?? 
-                         MassAutumn_tm1 = mean(dataFecUnscld$MassAutumn_tm1, na.rm = T),
+# now we want to advance phenology ONLY by 19 days. Need to divide 19 by SD of original variable  (mean = 0)
+env.future <- data.frame(TWin = mean(df_fec$TWin, na.rm = T), 
+                         PWin = mean(df_fec$PWin, na.rm = T),
+                         MassAutumn_tm1 = mean(df_fec$MassAutumn_tm1, na.rm = T), 
+                         T.WIN = mean(df_surv$T.WIN , na.rm = T), 
+                         GPPsurvT = mean(df_surv$GPPsurvT, na.rm = T) - 19/SD.surv["GPPsurvT"],
                          pred = "0")
+
 
 
 
